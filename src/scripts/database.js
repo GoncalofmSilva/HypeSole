364ac6cd97fe6dfc97311e58e3c280bf1bd1378e
GoncalofmSilva <95281891+GoncalofmSilva@users.noreply.github.com>
2024-04-02 01:08:26 +0100

    dotenv

--- src/scripts/database.js
+++ src/scripts/database.js
@@ -1,11 +1,10 @@
 import mysql from 'mysql';
-import { DB_HOST, DB_USER, DB_PASS, DB_NAME } from './server.js';
 
 const con = mysql.createConnection({
-  host: DB_HOST,
-  user: DB_USER,
-  password: DB_PASS,
-  database: DB_NAME
+  host: "localhost",
+  user: "root",
+  password: "Goncalo20",
+  database: "HypeSole"
 });
 
 con.connect((err) => {
